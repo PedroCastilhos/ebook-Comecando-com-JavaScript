1ac5b790b87b48d969e0dc5478bc191fffde2400
PedroCastilhos <qscdurant8>
2021-08-27 10:52:35 -0300

    Atividades

---
+++ atividade-01.js
@@ -0,0 +1,6 @@
+// No dev tolls
+
+var nome = prompt("Qual é o seu nome" , "Digite aqui:")
+var cidade = prompt("Você mora em que cidade?", "Digite a cidade aqui")
+
+console.log("Olá ", nome, " Tudo bem? Como está o tempo em ", cidade )
---
+++ atividade-02.js
@@ -0,0 +1,21 @@
+/*
+
+Operadores aritméticos
+
++  Adição
+-  Subtração
+/  Divisão
+*  Multiplicação
+
+%  Módulo(pega o resto da divisão e não o resultado)
+** Exponenciação 9um número elevado a outro)
+
+Operadores relacionais
+
+== Verifica a igualdade entre dois valores
+!= Verifica se dois valores são diferentes
+>  Verifica se um valor é menor que outro
+>= Verifica se um valor é maior ou igual a outro
+<= Verifica se um valor é menor ou igual a outro
+
+*/
